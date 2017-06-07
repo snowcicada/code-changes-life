9fa68d6ea1f86e08310336cca6679b76984e1b9f
snowcicada <snowcicadas@gmail.com>
2017-06-07 10:30:21 +0800

    remove tip for covering file

--- copy-wechat-video/copy_wechat_video.bat
+++ copy-wechat-video/copy_wechat_video.bat
@@ -1,5 +1,5 @@
 :start
 
-xcopy /d/e/c/i/h/r "C:\Users\guozs\Documents\WeChat Files\gzshun\Video" "E:\small_video\%date:~0,4%%date:~5,2%%date:~8,2%"
+xcopy /d/e/c/i/h/r/Y "C:\Users\guozs\Documents\WeChat Files\gzshun\Video" "E:\small_video\%date:~0,4%%date:~5,2%%date:~8,2%"
 choice /t 30 /d y /n >nul
 goto start
